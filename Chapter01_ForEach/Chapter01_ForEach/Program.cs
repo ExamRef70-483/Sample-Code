5ae1f7fc574604542e3a7b108335f27038d5c6f0
Rob Miles <rob@robmiles.com>
2018-02-07 19:24:19 +0000

    Added Invoke

---
+++ Chapter01_ForEach/Chapter01_ForEach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Chapter01_ForEach
+{
+    class Program
+    {
+        static void WorkOnItem(object item)
+        {
+            Console.WriteLine("Started working on: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished working on: " + item);
+        }
+
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 500);
+            Parallel.ForEach(items, item =>
+            {
+                WorkOnItem(item);
+            });
+
+            Console.WriteLine("Finished processing. Press a key to end.");
+            Console.ReadKey();
+        }
+    }
+}

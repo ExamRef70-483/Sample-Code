ca5078e00e8fdd109581ac59e1ff8bdd8bf2ef3c
Rob Miles <rob@robmiles.com>
2018-03-29 20:48:40 +0100

    Skill 2-5 sample code

---
+++ LISTING 2-63 Garbage collection/LISTING 2-63 Garbage collection/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LISTING_2_63_Garbage_collection
+{
+    class Person
+    {
+        long[] personArray = new long[1000000];
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( long i=0;i<100000000000;i++)
+            {
+                Person p = new Person();
+                System.Threading.Thread.Sleep(3);
+            }
+        }
+    }
+}
---
+++ LISTING 2-65 The dispose pattern/LISTING 2-65 The dispose pattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LISTING_2_65_The_dispose_pattern
+{
+    class ResourceHolder : IDisposable
+    {
+
+        // Flag to indicate when the object has been
+        // disposed
+        bool disposed = false;
+
+        public void Dispose()
+        {
+            // Call dispose and tell it that
+            // it is being called from a Dispose call
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            // Give up if already disposed
+            if (disposed)
+                return;
+
+            if(disposing)
+            {
+                // free any managed objects here
+            }
+
+            // Free any unmanaged objects here
+        }
+
+        ~ResourceHolder()
+        {
+            // Dispose only of unmanaged objects
+            Dispose(false);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResourceHolder r = new ResourceHolder();
+
+            r.Dispose();
+        }
+    }
+}

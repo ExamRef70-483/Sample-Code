acef05b35cff95afea6023ea6e3406d046c2c8a0
Rob Miles <rob@robmiles.com>
2018-05-10 15:07:37 +0100

    Skill 3.4 added

---
+++ LISTING 3-32 Code optimization/LISTING 3-32 Code optimization/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LISTING_3_32_Code_optimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 99;
+            int j = 100;
+            Console.WriteLine("The value in j is {0} ", j);
+        }
+    }
+}

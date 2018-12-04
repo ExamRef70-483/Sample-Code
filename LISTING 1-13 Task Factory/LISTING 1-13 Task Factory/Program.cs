d66d3fad682717f0a0d4f49b6b63af7e338ebd1c
Rob Miles <rob@robmiles.com>
2018-12-04 17:38:22 +0000

    Removed spurious Listing 1-13
    Listing 1-13 Task Factory was included in error and was incomplete. I've completed the example and added explaining text.

--- LISTING 1-13 Task Factory/LISTING 1-13 Task Factory/Program.cs
+++ LISTING 1-13 Task Factory/LISTING 1-13 Task Factory/Program.cs
@@ -1,26 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace LISTING_1_13_Task_Factory
+
+// This is a bonus example program that shows how to use the TaskFactory object
+// Normally you'd use Task.Factory.StartNew or the thread pool in preference
+// Task.Factory.StartNew is used in Listing 1-17 and the thread pool is described in Listing 1-28
+
+// The example creates a task factory and uses it to create a task that runs until it completes.
+// If you press enter before five seconds have elapsed the task is terminated and the program ends. 
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CancellationToken cancl = new CancellationToken();
+            // Used to allow us to control the an executing task
+            CancellationTokenSource cts = new CancellationTokenSource();
 
-            TaskFactory factory = new TaskFactory(TaskCreationOptions.AttachedToParent,
-                TaskContinuationOptions.None);
-                cancl,
-                TaskContinuationOptions.AttachedToParent,
-                TaskContinuationOptions.LongRunning, null);
+            // Create the task factory
 
-            t.StartNew()
-            t<int>.StartNew()
+            TaskFactory factory = new TaskFactory(
+               cts.Token,
+               TaskCreationOptions.PreferFairness,
+               TaskContinuationOptions.ExecuteSynchronously,
+               null); // custom scheduler could go here. 
+
+            // Use the factory to create a new Task running do work
+            Task t2 = factory.StartNew(() => DoWork());
+
+            Console.WriteLine("Press enter to dispose of the task");
+            Console.ReadLine();
+
+            Console.WriteLine("Disposing of the task");
+
+            // Cancel the task
+            // dispose of the task using the cancellation token
+            cts.Dispose();
+
+        }
+
+        static void DoWork()
+        {
+            Console.WriteLine("Doing work..");
+
+            int total = 0;
+
+            for (int i = 0; i < 5000; i++)
+            {
+                Thread.Sleep(1);
+                total += i;
+            }
+
+            Console.WriteLine("Result: " + total);
         }
     }
 }
